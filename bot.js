5289abc76efadc8c792201ddafc4b558822a8160
Brenn Constant <brenn.constant@gmail.com>
2019-11-22 11:54:32 +0100

    fix

--- bot.js
+++ bot.js
@@ -828,7 +828,7 @@ class Fighter {
 						randomGod = Math.floor(Math.random()*PRIEST_ROLES.length)
 						nbTries += 1;
 					}
-					this.godList.add(PRIEST_ROLES[randomGod]);
+					this.godList.push(PRIEST_ROLES[randomGod]);
 					BATTLE_CHANNEL.send(this.user.username + " becomes a " + PRIEST_ROLES[randomGod] + " thanks to his charisma !");
 				}
 				if (this.godList.indexOf(GOD_PP1_PRIEST) > -1) { // Mongo
