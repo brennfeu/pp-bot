f504b8678dd9c50c7eb98cd7d0076b5676643995
Brenn Constant <brenn.constant@gmail.com>
2019-12-18 01:06:54 +0100

    removed alien pp message

--- bot.js
+++ bot.js
@@ -1705,7 +1705,6 @@ class Fighter {
 		// Alien PP
 		if (this.isAlienPP && _punch) {
 			this.duel.getOppOf(this).bleedDamage += 3;
-			this.duel.addMessage(this.duel.getOppOf(this).user.username + " gets hurt by " + this.user.username + "'s alien PP");
 		}
 	}
 
