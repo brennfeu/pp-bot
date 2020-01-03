ff10a7b3ab3f8c1276cc264f6635e66b8422ac9c
Brenn Constant <brenn.constant@gmail.com>
2020-01-03 10:47:55 +0100

    console log test

--- bot.js
+++ bot.js
@@ -2972,10 +2972,10 @@ class Duel {
 	checkStandSummon() {
 		this.bothFightersAction(function(_fighter) {
 			var check = false;
-			console.log(_fighter.usedMoves);
 			for (var i in STAND_SUMMONS) {
 				check = true;
 				for (var j in STAND_SUMMONS[i]) {
+					console.log(STAND_SUMMONS[i][j] + " // " + _fighter.usedMoves[_fighter.usedMoves.length - (1 + j)]);
 					if (STAND_SUMMONS[i][j] != _fighter.usedMoves[_fighter.usedMoves.length - (1 + j)]) {
 						check = false;
 					}
