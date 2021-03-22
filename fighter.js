2a14999282636625d407b1786b79d5cb627a6708
Brenn Constant <brenn.constant@gmail.com>
2021-03-22 20:46:43 +0100

    test

--- fighter.js
+++ fighter.js
@@ -6,6 +6,8 @@ var Fighter = class {
 
 		// set variables
 		this.idUser = _idUser;
+		console.log(_idUser);
+		console.log(getDuel(_idDuel).GUILD.members);
 		this.guildUser = getDuel(_idDuel).GUILD.members.get(_idUser);
 		this.user = this.guildUser.user;
 		updatePlayer(this.user.id, this.user.username.secureXSS())
