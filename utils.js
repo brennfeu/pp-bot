0e68aeba7735e9d5d73df61c19578b6ea800b414
Brenn <brenn.constant@gmail.com>
2022-07-12 13:01:45 +0200

    fix

--- utils.js
+++ utils.js
@@ -160,7 +160,7 @@ function sciText(_texte) {
     return l.join(" ");
 }
 function romanizeNumber(num) {
-  var lookup = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1},roman = '',i;
+  var lookup = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1,N:0},roman = '',i;
   for ( i in lookup ) {
     while ( num >= lookup[i] ) {
       roman += i;
