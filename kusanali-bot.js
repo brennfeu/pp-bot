6428a5919ca868bc5688161e9f36c4ba37036101
Brenn <brenn.constant@gmail.com>
2022-08-20 00:33:27 +0200

    fix

--- kusanali-bot.js
+++ kusanali-bot.js
@@ -1,8 +1,7 @@
 function kusanaliBotMessage(_message) {
-    channel.createWebhook('Some-username', {
+    _message.channel.createWebhook('Some-username', {
         name: 'Kusana-Leaks',
-    	avatar: 'https://cdn.discordapp.com/attachments/667337519477817363/996062528973058100/unknown.png',
-        channel: _message.channel.id
+    	avatar: 'https://cdn.discordapp.com/attachments/667337519477817363/996062528973058100/unknown.png'
     })
 	.then(function(_webhook) {
         webhookClient = new Discord.WebhookClient(_webhook.id, process.env.KUSANALI_TOKEN);
