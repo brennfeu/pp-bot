8c516ded9a0d981ba46ae249d54102ef71b59db3
Brenn <brenn.constant@gmail.com>
2022-08-20 22:38:57 +0200

    pfp test

--- kusanali-bot.js
+++ kusanali-bot.js
@@ -65,7 +65,7 @@ function k_sendMessage(_title, _message, _channel, _avatar = undefined) {
 
         webhookClient.send('', {
             username: 'Loli des Fleurs',
-            avatarURL: 'https://cdn.discordapp.com/attachments/715322091804819486/1010648101163380837/unknown.png',
+            avatarURL: 'https://cdn.discordapp.com/attachments/715322091804819486/1010649092805890089/unknown.png',
             embeds: [ embedMessage ]
         });
 
