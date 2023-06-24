92ecfdf40adc2318f57ecdbaac4108bfe27065a0
Brenn Constant <brenn.constant@gmail.com>
2023-06-24 19:08:02 +0200

    fix

--- kusanali-bot.js
+++ kusanali-bot.js
@@ -30,7 +30,7 @@ function kusanaliBotMessage(_message) {
         else word = dailies[i].word;
 
         var nb = _message.content.trim().split(word).length-1;
-        for (var i in Array.from(Array(nb).keys())) k_increaseMissionProgress(_message.author.id, "use_word", _message.channel, dailies);
+        for (var i in Array.from(Array(nb).keys())) k_increaseMissionProgress(_message.author.id, dailies[i].type, _message.channel, dailies);
     }
 
     // thumbs up nahida
