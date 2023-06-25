13a8e520041aae8dc116d1f7bc69c103531a44f7
Brenn <brenn.constant@gmail.com>
2023-06-25 23:00:25 +0200

    fix

--- kusanali-bot.js
+++ kusanali-bot.js
@@ -112,7 +112,7 @@ function kusanaliBotMessage(_message) {
 
             var date = k_getUserDoubleXpDate(_message.author.id);
             if (date < k_getToday()) date = k_getToday();
-            date += 60*60*24; // +1 day
+            date += 86400; // +1 day
 
             var date_message = formatDate(new Date(date));
             executeQuery('UPDATE Player SET k_doublexp = "' + date + '", k_mora = (k_mora-200000) WHERE id = ' + _message.author.id);
@@ -292,8 +292,7 @@ function k_checkRoles(_message) {
 }
 
 function k_getToday() {
-    var today = new Date();
-    return today.getTime();
+    return Math.round(Date.now() / 1000);
 }
 function k_getTodayDate() {
     var today = new Date();
